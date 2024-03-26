b0914a589c43164525dff70b3c4a1ad83fdc0ca8
Juan Jose Romero Montes <claseinfojuanjose@gmail.com>
2024-03-26 19:27:41 +0100

    Set up
    Se ha seteado el tml/css, añadiendose los iconos necesarios. También se han eliminado varios archivos que no se usaban

--- src/App.js
+++ src/App.js
@@ -1,6 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
 
+function App() {
+  return (
+    <div className="App">
+
+      <div className="Header">
+        <img src="./converterLogoPurple.png" className="AppLogo" alt=""/>
+        <h1>unit converter</h1>
+      </div>
+      
+      <div className="Separator"> <hr></hr> </div>
+
+      <div className="Body">
+        <div className="Box" id="ConverterScreen">
+
+          <div className="ConverterRow"> {/*Top row*/}
+            <div className="ConverterCell" id="ConverterTitle">
+              <p>convert</p>
+            </div>
+          </div>
+          
+          <div className="ConverterRow"> {/*Middle row*/}
+            <div className="ConverterCell" id="SelectCell">
+              <select className="ConverterSelect">
+                <option>km → miles</option>
+                <option>miles → km</option>
+                <option>ft → m</option>
+                <option>m → ft</option>
+                <option>cm → inches</option>
+                <option>inches → cm</option>
+              </select>
+            </div>
+            <div className="ConverterCell">
+              <img src="./converterLogoWhite.png" className="ConverterImage" alt="" />
+            </div>
+            <div className="ConverterCell" id="InputCell">
+              <input className="ConverterInput"></input>
+            </div>
+            <div className="ConverterCell" id="Unit1Cell">
+            <p>unit1</p>
+            </div>
+          </div>
+          
+          <div className="ConverterRow"> {/*Bottom row*/}
+            <div className="ConverterCell" id="SaveConvertionCell">
+            <img src="./heart.png" className="ConverterImage" alt="" />
+            </div>
+
+            <div className="ConverterCell" id="ConverterTitle">
+            <p>99.99</p>
+            </div>
+
+            <div className="ConverterCell" id="AlignRigth">
+            <p>unit2</p>
+            </div>
+          </div>
+        </div>
+        
+        <div className="SavedGrid">
+          <div> <p className="SavedTitle">saved</p> </div>
+          <div className="Box" id="SavedConvertions"> 
+            <div className="alignLeft"> <h3>Conversion</h3> </div>
+            <div className="alignRigth"> <img src="./delete.png" className="ConverterImage" alt=""/> </div>
+          </div>
+          <div className="Box" id="SavedConvertions"> 
+            <div className="alignLeft"> <h3>Conversion</h3> </div>
+            <div className="alignRigth"> <img src="./delete.png" className="ConverterImage" alt=""/> </div>
+          </div>
+          <div className="Box" id="SavedConvertions"> 
+            <div className="alignLeft"> <h3>Conversion</h3> </div>
+            <div className="alignRigth"> <img src="./delete.png" className="ConverterImage" alt=""/> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +100,4 @@ function App() {
     </div>
   );
 }
-
-export default App;
+*/
